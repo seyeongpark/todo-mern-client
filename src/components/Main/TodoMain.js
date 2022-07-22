48db337020cb457d8940c9e1081ad5b5d9c6441d
SeyeongPark <lynn.yeong.102@gmail.com>
2022-07-21 22:46:58 -0400

    fix api address error

--- src/components/Main/TodoMain.js
+++ src/components/Main/TodoMain.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react';
 import './TodoMain.css';
 
-const API_BASE = "https://sp-todo-mern.herokuapp.com";
+const API_BASE = "https://sp-todo-mern.herokuapp.com/api";
 // const API_BASE = "http://localhost:3001/api";
 
 function TodoMain() {
